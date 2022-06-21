979fab1331e2db6bfb36203df80d5fc3a7b0297e
Ayrton <devayrtonsbarreto@gmail.com>
2022-06-21 17:38:42 -0300

    Adicionado driver do MySQL e classe para realizar a comunicação com o BD

---
+++ app/src/main/java/TodoApp/util/ConnectionFactory.java
@@ -0,0 +1,33 @@
+package TodoApp.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
+    public static final String USER = "root";
+    public static final String PASS = "q1w2e3";
+    
+    public static Connection getConnection(){
+        try {
+            Class.forName(DRIVER); //carrega o driver que vamos utilizar para dentro da nossa aplicação
+            return DriverManager.getConnection(URL,USER,PASS); 
+            //faz a comunicação com o BD levando em consideração esses parâmetros informados e retorna a conexão
+        } catch (Exception e) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", e);
+        }
+    }
+    
+    public static void closeConnection(Connection connection){
+        try {
+            if (connection != null) { //verifica se a conexão existe
+                connection.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao fechar a conexão com o banco de dados", e);
+        }
+    }
+                                    
+}
