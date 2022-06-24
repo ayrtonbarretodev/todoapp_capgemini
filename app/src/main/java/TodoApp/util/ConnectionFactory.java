706d73d6c9101fa14b6e84798718fedeaa5e028c
Ayrton <devayrtonsbarreto@gmail.com>
2022-06-24 15:01:26 -0300

    mudança no nome do driver mysql

--- app/src/main/java/TodoApp/util/ConnectionFactory.java
+++ app/src/main/java/TodoApp/util/ConnectionFactory.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class ConnectionFactory {
     
-    public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/todoapp";
     public static final String USER = "root";
     public static final String PASS = "q1w2e3";
     
